e28e5542552492dbb21ddc8773989b8258e543dd
JosephJvB <joevanbo@pm.me>
2018-08-11 23:13:55 +1200

    noice(client): Rendering tableNames onClick

--- client.js
+++ client.js
@@ -1,20 +1,35 @@
 const render = require('react-dom').render
 const h = require('react-hyperscript')
+const { compose, withStateHandlers } = require('recompose')
 
 const rootElement = document.getElementById('welcome!')
-const rootComponent = h('div', {}, [
-		h('h3', 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen'),
-		h('button', {
-			onClick: () => fetchTest('/api', {method: 'get'})
-		}, 'FETCH TESTER')
-	])
+const rootComponent = compose(
+	withStateHandlers(
+		() => ({
+			data: null
+		}),
+		{
+			loadData: () => (res) => ({data: res.data})
+		}
+	))(props => h('div', {}, [
+				h('h3', 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen'),
+				h('button', {
+					onClick: () => fetchTest('/api', {method: 'get'}, props.loadData)
+				}, 'FETCH TESTER'),
+				h('div', {}, [
+					props.data && props.data.map(i => {
+						return h('h1', {key: Object.keys(i)[0]}, Object.keys(i)[0])
+					})
+				])
+			])
+	)
 
-render(rootComponent, rootElement)
+render(h(rootComponent), rootElement)
 
 
-function fetchTest(url, options) {
+function fetchTest(url, options, handler) {
 	return fetch(url, options)
 		.then(res => res.json())
-		.then(console.log)
+		.then(handler)
 		.catch(console.log)
 }
