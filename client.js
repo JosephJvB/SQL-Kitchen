684e3b05b829299dde57eab41c361d7ff7254e2f
JosephJvB <joevanbo@pm.me>
2018-08-11 23:33:31 +1200

    fix(client): fix indentation

--- client.js
+++ client.js
@@ -11,18 +11,19 @@ const rootComponent = compose(
 		{
 			loadData: () => (res) => ({data: res.data})
 		}
-	))(props => h('div', {}, [
-				h('h3', 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen'),
-				h('button', {
-					onClick: () => fetchTest('/api', {method: 'get'}, props.loadData)
-				}, 'FETCH TESTER'),
-				h('div', {}, [
-					props.data && props.data.map(i => {
-						return h('h1', {key: Object.keys(i)[0]}, Object.keys(i)[0])
-					})
-				])
-			])
 	)
+)(props => h('div', {}, [
+		h('h3', 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen'),
+		h('button', {
+			onClick: () => fetchTest('/api', {method: 'get'}, props.loadData)
+		}, 'FETCH TESTER'),
+		h('div', {}, [
+			props.data && props.data.map(i => {
+				return h('h1', {key: Object.keys(i)[0]}, Object.keys(i)[0])
+			})
+		])
+	])
+)
 
 render(h(rootComponent), rootElement)
 
