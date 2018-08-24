38262960439a06f402be92d25b438bc836140a6e
JosephJvB <joevanbo@pm.me>
2018-08-25 00:02:34 +1200

    prog(table): can render items for a table on tablepage have a selector!!

--- table-page.js
+++ table-page.js
@@ -1,7 +1,33 @@
 const h = require('react-hyperscript')
 const { connect: connectRedux } = require('react-redux')
 
+// look at tabledata from redux, and render column titles - then data for each item under those headings
+
 module.exports = connectRedux(
-  (state) => ({}), // mapper
+  mrGetter, // mapper
   {} // wrap actions in dispatch
-)((props) => h('h1', 'TABLE PAGE'))
+)(({
+  // props
+  itemData,
+}) => h('ul', [
+  itemData.map((item, i) => h('li', {key: i},  '|-- ' + item.join(' --|-- ') + ' --|'))
+  ])
+)
+
+// first bootleg selector in this project 😂 👌 who did this lmaooo
+function mrGetter (state) {
+  const smoovData = state.tableData.reduce((acc, item, i) => {
+    if(i === 0) acc.push(Object.keys(item))
+    else acc.push(Object.keys(item).map(val => item[val]))
+    return acc
+  }, [])
+  return { itemData: smoovData }
+}
+/*
+  itemData = [
+    [], - meta
+    [], - item1,
+    [], - item2
+    ...
+  ]
+*/
