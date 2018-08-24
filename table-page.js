4a5c4396375803ad393a8da6a5b27ccb433470ba
JosephJvB <joevanbo@pm.me>
2018-08-25 00:09:58 +1200

    fix(table): fix selector and add comments/pictures

--- table-page.js
+++ table-page.js
@@ -1,8 +1,6 @@
 const h = require('react-hyperscript')
 const { connect: connectRedux } = require('react-redux')
 
-// look at tabledata from redux, and render column titles - then data for each item under those headings
-
 module.exports = connectRedux(
   mrGetter, // mapper
   {} // wrap actions in dispatch
@@ -10,6 +8,7 @@ module.exports = connectRedux(
   // props
   itemData,
 }) => h('ul', [
+  // print bootleg table format
   itemData.map((item, i) => h('li', {key: i},  '|-- ' + item.join(' --|-- ') + ' --|'))
   ])
 )
@@ -17,15 +16,23 @@ module.exports = connectRedux(
 // first bootleg selector in this project 😂 👌 who did this lmaooo
 function mrGetter (state) {
   const smoovData = state.tableData.reduce((acc, item, i) => {
-    if(i === 0) acc.push(Object.keys(item))
-    else acc.push(Object.keys(item).map(val => item[val]))
+    if(i === 0) acc.push(Object.keys(item)) // push meta data just one time
+    acc.push(Object.keys(item).map(val => item[val])) // push item data for all items
     return acc
   }, [])
   return { itemData: smoovData }
 }
 /*
+  tableData comes in looking like [{col: value, col: value, col: value}, {}, ...]
+  I want to render on the page like:
+  |---col---|---col---|---col---| - from first item
+  |---val---|---val---|---val---| - from first item
+  |---val---|---val---|---val---| - from second item
+  ...etc
+
+  create object like this:
   itemData = [
-    [], - meta
+    [], - meta (from item 1) all items have same meta
     [], - item1,
     [], - item2
     ...
