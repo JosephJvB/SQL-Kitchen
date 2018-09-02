9c228e92b8a91612194a72567f7c0800929cb01c
JosephJvB <joevanbo@pm.me>
2018-09-02 13:30:35 +1200

    style(homePage): basic inline styling to move towards wireframes

--- front-of-house/home-page.js
+++ front-of-house/home-page.js
@@ -21,21 +21,33 @@ module.exports = connectRedux(
 	homeData,
 	setHomeData,
 	setTableData,
-}) => h('div', {}, [
-		h('h3', 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen'),
-		h('button', {
-			onClick: () => joeFetch(
-				'/api/home',
-				{method: 'get'},
-				{success: setHomeData}
-				)
-		}, 'FETCH TESTER'),
-		h('div', {}, [
+}) => h('div', {style: {width: '80%', margin: 'auto'}}, [
+		// HEADER
+		h('div', {style: {display: 'flex', flexDirection: 'row'}, id: 'HEADER'}, [
+			h('h3', {style: {marginRight: '2rem'}}, 'Welcome madame(s) or monsieur(s) a le SQL-Kitchen:'),
+			h('p', {style: {backgroundColor: '#2f3640', color: '#4cd137', border: '3px solid #718093', padding: '0.5rem 2rem'}}, '$:this will be the query bar'),
+		]),
+		h('div', {style: {display: 'flex', flexDirection: 'row'}, id: 'GET_TABLES'}, [
+			h('h1', {
+				style: {marginRight: '2rem', cursor: 'pointer',},
+				onClick: () => joeFetch(
+					'/api/home',
+					{method: 'get'},
+					{success: setHomeData}
+					)
+			}, 'GET_TABLES:'),
+			// h('button', {style: {padding: '0 2rem'}},  'ADD NEW')
+		]),
+		// TABLE_DATA ELEMENTS
+		h('div', {id: 'TABLES_META_DATA'}, [
 			homeData.map(({tableName, columnData}) => h('div', {
 				key: tableName,
-				style: { border: '2px dotted red' },
+				style: { border: '2px dotted red', padding: '0.5rem 2rem' },
 			}, [
 				h('h1', {
+					style: {
+						cursor: 'pointer'
+					},
 					onClick: () => {
 					changeView({location: 'TABLE', params: tableName})
 					return joeFetch(
