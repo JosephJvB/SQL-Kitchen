a7c87245035f5a15878404898f4bd6ac34c5fd37
JosephJvB <joevanbo@pm.me>
2018-10-14 17:48:59 +1300

    fix(linebreaks): move n width value inside function so it is evaluated on each keypress, rather than just one time

--- front-of-house/util/linebreak.js
+++ front-of-house/util/linebreak.js
@@ -1,12 +1,11 @@
-const n = Number((window.innerWidth / 23).toString().split('.')[0])
-const reggie = new RegExp(`.{1,${n}}`, 'g')
-
 // split, chunk, join
 // if any line is too large, chunk it
-// ternary to handle 'no-text' case
-module.exports = (text) => text
-  ? text
+module.exports = (text) => {
+  const n = Number((window.innerWidth / 23).toString().split('.')[0])
+  const reggie = new RegExp(`.{1,${n}}`, 'g')
+  if(!text) return ''
+  return text
     .split('\n')
     .reduce((acc, line) => acc.concat(line.length >= n ? line.match(reggie) : line), [])
     .join('\n')
-  : ''
+}
